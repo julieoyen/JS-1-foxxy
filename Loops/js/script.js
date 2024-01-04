c9d3c4a420062a4601059846ce991ddf64d09f18
julieoyen <juloye51070@stud.noroff.no>
2024-01-04 14:16:42 +0100

    loops and added folder

---
+++ Loops/js/script.js
@@ -0,0 +1,17 @@
+
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+//looping or iteration
+
+// i is now 1
+// will + 1 each round
+
+for (let i = 1; i <= 5; i++ ){  // will stop when reaching 5
+    console.log("it is now " + i);
+
+}
+
