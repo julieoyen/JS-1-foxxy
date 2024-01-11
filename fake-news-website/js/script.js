cca83ab46c1005bd4938b3b27d0d33f1612474f5
julieoyen <juloye51070@stud.noroff.no>
2024-01-11 12:02:48 +0100

    fake news website and within range exaple condition

---
+++ fake-news-website/js/script.js
@@ -0,0 +1,31 @@
+let news = [
+    {
+        headine: "Watch Hesh landing on the moon",
+        summary: "He landed with his superjet"
+    },
+    {
+        headine: "Watch minions",
+        summary: "Minions ips gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii. Baboiii hana dul sae bappleees pepete hana dul sae po kass po kass baboiii. Belloo! hahaha baboiii poopayee hahaha belloo! La bodaaa bee do bee do bee do chasy. Pepete poopayee tank yuuu! Butt la bodaaa wiiiii aaaaaah ti aamoo! Poulet tikka masala. Tatata bala tu daa ti aamoo! Poulet tikka masala poopayee wiiiii bappleees hana dul sae ti aamoo! Jeje belloo!"
+    }, 
+    {
+        headine: "Watch the GRU scream",
+        summary: "Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii. Baboiii hana dul sae bappleees pepete hana dul sae po kass po kass baboiii. Belloo! hahaha baboiii poopayee hahaha belloo! La bodaaa bee do bee do bee do chasy. Pepete poopayee tank yuuu! Butt la bodaaa wiiiii aaaaaah ti aamoo! Poulet tikka masala. Tatata bala tu daa ti aamoo! Poulet tikka masala poopayee wiiiii bappleees hana dul sae ti aamoo! Jeje belloo!"
+    },
+   
+];
+
+console.log(news);
+
+
+const newsContainer = document.querySelector(".news")
+console.log(newsContainer)
+
+var newsLength = news.length
+console.log(newsLength)
+
+for(let i = 0; i < newsLength; i++ ){
+    console.log(news[i])
+}
+
+
+
