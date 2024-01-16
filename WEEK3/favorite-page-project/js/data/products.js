e6f5e629ef31c24d4d41f3efb5920b920083e0e0
julieoyen <juloye51070@stud.noroff.no>
2024-01-16 15:16:24 +0100

    Week 3 day 2

---
+++ WEEK3/favorite-page-project/js/data/products.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1, 
+        name: "iPhone 15",
+        price: 10000
+    },
+    {
+        id: 2,
+        name: "iPhone 14",
+        price: 10000
+    },
+    {
+        id: 3,
+        name: "iPhone 13",
+        price: 10000
+    }
+];
