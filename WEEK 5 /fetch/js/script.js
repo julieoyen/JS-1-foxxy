e2c6cab14a7c38587a36bb68e2d6b8973a8cb780
julieoyen <juloye51070@stud.noroff.no>
2024-02-02 12:22:06 +0100

    fetch

--- WEEK 5 /fetch/js/script.js
+++ WEEK 5 /fetch/js/script.js
@@ -1,26 +1,27 @@
-// // data
+// // // data
 
-// let todo1Name = "Learn JS functions"
-// let todo1Done = true
-// let todo1Date = "Tuesday"
-// let todo2Name = "Learn JS arrays"
-// let todo2Done = false
-// let todo2Date = "wednesday"
+// // let todo1Name = "Learn JS functions"
+// // let todo1Done = true
+// // let todo1Date = "Tuesday"
+// // let todo2Name = "Learn JS arrays"
+// // let todo2Done = false
+// // let todo2Date = "wednesday"
 
 
-// //this is more intuitive
-// let todo1 ={
-//     name: "Learn JS functions",
-//     done: true,
-//     date: "Tuesday"
-// }
+// // //this is more intuitive
+// // let todo1 ={
+// //     name: "Learn JS functions",
+// //     done: true,
+// //     date: "Tuesday"
+// // }
 
-// console.log(todo1.name)
+// // console.log(todo1.name)
 
 // let todos = []
 
+
 // let todo = fetch(
-//     'https://jsonplaceholder.typicode.com/users')
+//     'https://jsonplaceholder.typicode.com/todos')
 //     .then(function(httpResponse){
 //         console.log(httpResponse)
 //         // How do i extract the data
@@ -30,19 +31,74 @@
 //         todos = todoResult
 //     });
 
+// console.log("this is a test")
+
+// // POST
+// fetch ('https://jsonplaceholder.typicode.com/todos',
+// {
+//     method: "POST",
+//     body: JSON.stringify({
+//         title: "foo",
+//         body: "bar",
+//         userId: 1,
+//     }),
+//     headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//     },
+// }).then((httpResponse) => console.log(httpResponse))
+
 //     console.log("other stuff") //loads faster than the above code
 
+//     // show it to the users
 
-    // show it to the users
+
+//     // DELETE 
+
+//     fetch ('https://jsonplaceholder.typicode.com/todos/1',
+//     {
+//         method: "DELETE",
+//         headers: {
+//             "Content-type": "application/json; charset=UTF-8",
+//         },
+//     }).then((httpResponse) => console.log(httpResponse))
+
+    
 
 
-let joke = []
 
-let jokes = fetch ('https://official-joke-api.appspot.com/random_joke')
-.then(function(httpResponse){
-    console.log(httpResponse)
-    return httpResponse.json()
-})
-.then(jokeResult => {
-    joke = jokeResult
-});
+
+// // let joke = []
+
+// // let jokes = fetch ('https://official-joke-api.appspot.com/random_joke')
+// // .then(function(httpResponse){
+// //     console.log(httpResponse)
+// //     return httpResponse.json()
+// // })
+// // .then(jokeResult => {
+// //     joke = jokeResult
+// // });
+
+
+// // let todos = [];
+
+// // fetch ('https://jsonplaceholder.typicode.com/todos')
+// // .then (function (httpResponse){
+// //     console.log(httpResponse)
+// // })
+// // .then((data) => console.log(data));
+
+
+
+
+// // //  ----> go fetch I promise to forward to .then
+// // // -----> await going to fetch on this line of code
+// // // -----> i will wait until thats done and then i will forward to the next line
+
+
+// // async function getTodos(){
+// //     let httpResponse = await fetch ('https://jsonplaceholder.typicode.com/todos')
+// //     let data = await httpResponse.json();
+// //     console.log(data);
+// // }
+
+// // getTodos();
