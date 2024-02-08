9a61ef8619ad2fa98b08f4a2c90b99326aad1026
julieoyen <juloye51070@stud.noroff.no>
2024-02-08 14:28:03 +0100

    modules week 6

---
+++ WEEK 6/modules/js/maths.js
@@ -0,0 +1,3 @@
+export default function square(number) {
+  return number * number;
+}
---
+++ WEEK 6/modules/js/script.js
@@ -0,0 +1,16 @@
+import{square} from `./maths.js`
+
+
+let someNumber = 9; 
+
+
+//square it
+
+
+
+let squaredNumber = square(someNumber)
+
+console.log(squaredNumber)
+
+
+//#region lots of function
