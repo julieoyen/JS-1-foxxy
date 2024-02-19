e30ca6026b826721b0675691e2d11c16b247d251
julieoyen <juloye51070@stud.noroff.no>
2024-02-19 11:03:30 +0100

    Shelf help, collection, details, book features

--- WEEK 5 /shelf-help-demo/js/app.js
+++ WEEK 5 /shelf-help-demo/js/app.js
@@ -1,86 +1,122 @@
-// Need the shelf help JSON server to be running on http://localhost:3000
+import { paginate, paginateGross } from "./modules/bookFeatures.js";
 
-localStorage.setItem("message", " I just stored something in localStorage");
-
-// how do i access it the local storage
-// in console.log
-
-console.log(localStorage.getItem("message"));
-
-// if i want to clear the local storage
-localStorage.clear();
-
-// session storage clears itself out after you close
-sessionStorage.setItem("message", " I just stored something in localStorage");
-
+// !! Need the shelf help JSON server to be running on http://localhost:3000
 const bookListDiv = document.getElementById("book-list");
-const filterButton = document.getElementById("btnfilter");
+const filterButton = document.getElementById("btnFilter");
+const dropdownAuthorList = document.getElementById("authorList");
+const resetButton = document.getElementById("btnReset");
+const pageButtons = document.getElementById("pageButtons");
 
-filterButton.addEventListener("click", filterByAuthor);
+filterButton.addEventListener("click", () =>
+  filterByAuthor(dropdownAuthorList.value)
+);
+resetButton.addEventListener("click", () => {
+  bookListDiv.innerHTML = "";
+
+  for (const book of bookData) {
+    displayBook(book);
+  }
+});
 
 let bookData = [];
-
-// fetch the book data
-
+// fetch the book data.
 fetch("http://localhost:3000/books")
-  .then((response) => response.json())
+  .then((response) => {
+    return response.json();
+  })
   .then((bookResultData) => {
     bookData = bookResultData;
-    for (const book of bookData) {
+
+    const paginatedBooks = paginateGross(bookData, 3);
+
+    for (const book of paginatedBooks[0]) {
       displayBook(book);
     }
+
+    // create buttons
+    // each button should display two books
+    let counter = 1;
+    for (const bookArray of paginatedBooks) {
+      const pageButton = document.createElement("button");
+      pageButton.innerText = counter;
+      counter++;
+      pageButton.addEventListener("click", () => {
+        bookListDiv.innerHTML = "  ";
+        for (const book of bookArray) {
+          displayBook(book);
+        }
+      });
+      pageButtons.appendChild(pageButton);
+    }
+
+    loadAuthorsIntoDropDown();
   });
 
-function displayBook(book) {}
-
+// Defines a function to display a book's information in the DOM
 function displayBook(book) {
   const bookDiv = document.createElement("div");
-  const bookTitleParagraph = document.createElement("p");
+  const bookTitlePara = document.createElement("p");
+  bookTitlePara.innerText = book.title;
   const bookImg = document.createElement("img");
-  const bookAuthor = document.createElement("author");
-
   bookImg.src = `http://localhost:3000/assets/images/${book.coverImg}`;
   bookImg.alt = "a picture of a book";
-
-  bookAuthor.innerText = book.author;
-  bookTitleParagraph.innerText = book.title;
-
+  bookImg.addEventListener("click", () => {
+    // navigate to details page
+    sessionStorage.setItem("selectedBook", JSON.stringify(book));
+    window.location.href =
+      "http://127.0.0.1:5500/shelf-help-demo-build/pages/details.html";
+  });
   bookDiv.appendChild(bookImg);
-  bookDiv.appendChild(bookTitleParagraph);
-  bookDiv.appendChild(bookAuthor);
+  bookDiv.appendChild(bookTitlePara);
   bookListDiv.appendChild(bookDiv);
 }
 
-// //example
-// let chair = {
-//     color: "Grey",
-//     height: 67,
-//     swivel(){
-//         console.log("That chair totally swivelled")
-//     }
-// }
-
-// chair.swivel()
-
-// ANOTHER EXAMPLE
-
-// let jsonBook = // add json
-
-// //converts JSON to a JS object
-// let bookJSObject = JSON.parse(jsonBook);
-
-// //makes it into json again
-
-// let jsonBook = JSON.stringify(bookJSObject)
-
 function filterByAuthor(authorToFilterBy) {
-  console.log("works");
+  try {
+    if (authorToFilterBy === "default") {
+      throw new Error("Bad Filter");
+    }
+    // filter the array by authors name
+    let filteredResults = [];
+
+    for (const book of bookData) {
+      if (book.author === authorToFilterBy) {
+        filteredResults.push(book);
+      }
+    }
+
+    // update the dom to show the new array
+
+    // clear the old list from DOM
+    bookListDiv.innerHTML = "";
+    // loop through the new filtered list
+    for (const book of filteredResults) {
+      // display/render each book
+      displayBook(book);
+    }
+  } catch (error) {
+    alert("Please select an author");
+  }
+}
+// Alternative function to create and insert the HTML to display a book but with templating
+function displayBookAlt(book) {
+  // Create the div element for the book
+  const bookDiv = document.createElement("div");
+  // Use template literals to construct the inner HTML of the bookDiv
+  bookDiv.innerHTML = `
+    <img src="http://localhost:3000/assets/images/${book.coverImg}" alt="a picture of a book">
+    <p>${book.title}</p>
+  `;
+  // Append the bookDiv to the bookListDiv
+  bookListDiv.appendChild(bookDiv);
 }
 
-//filter array
-
-let filteredResults = [];
-
-for (const book of bookData) {
-  if (book.author === authorToFilterBy) filteredResults.push(book);
+function loadAuthorsIntoDropDown() {
+  for (const book of bookData) {
+    // creat an option with author name
+    const authorOption = document.createElement("option");
+    authorOption.innerText = book.author;
+    // add it to dropdown
+    dropdownAuthorList.appendChild(authorOption);
+  }
 }
